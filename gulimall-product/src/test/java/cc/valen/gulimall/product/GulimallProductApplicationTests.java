1f9f64b009e0120b4318e229d3c5a612f1ed54ad
valen <dontleavemebuddy@gmail.com>
2020-06-29 14:00:18 +0800

    项目初始化

---
+++ gulimall-product/src/test/java/cc/valen/gulimall/product/GulimallProductApplicationTests.java
@@ -0,0 +1,13 @@
+package cc.valen.gulimall.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
